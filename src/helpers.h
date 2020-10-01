46cd2cc86822e0b91453395d69cd97ec94df25b8
alf130386 <35611821+alf130386@users.noreply.github.com>
2020-10-02 01:01:14 +0300

    added reverse order

--- src/helpers.h
+++ src/helpers.h
@@ -4,7 +4,7 @@
 #include <NeoPixelBus.h>
 #include <LittleFS.h>
 #include "recorder.h"
-#define VERSION "v_0.7.1"
+#define VERSION "v_0.7.2"
 
 #define UNI 35 // change this for setting node universe and last byte of IP Address************************************
 #define UNIVERSE 19 //actual universe for receiving DMX
